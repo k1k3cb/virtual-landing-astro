a87e6bf8c61f4825c9fe7dc6f88b469f6ede9b33
Enrique Comesaña <enriquecbar@gmail.com>
2024-03-26 15:45:13 +0100

    Initial commit

---
+++ src/constants/features.js
@@ -0,0 +1,47 @@
+import { BotMessageSquare } from "icons/bot-message-square.svg";
+import { BatteryCharging } from "icons/battery-charging.svg";
+import { Fingerprint } from "icons/fingerprint.svg";
+import { ShieldHalf } from "icons/shield-half.svg";
+import { PlugZap } from "icons/plug-zap.svg";
+import { GlobeLock } from "icons/globe-lock.svg";
+
+
+
+export const features = [
+    {
+      icon: <BotMessageSquare />,
+      text: "Drag-and-Drop Interface",
+      description:
+        "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
+    },
+    {
+      icon: <Fingerprint />,
+      text: "Multi-Platform Compatibility",
+      description:
+        "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
+    },
+    {
+      icon: <ShieldHalf />,
+      text: "Built-in Templates",
+      description:
+        "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
+    },
+    {
+      icon: <BatteryCharging />,
+      text: "Real-Time Preview",
+      description:
+        "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
+    },
+    {
+      icon: <PlugZap />,
+      text: "Collaboration Tools",
+      description:
+        "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
+    },
+    {
+      icon: <GlobeLock />,
+      text: "Analytics Dashboard",
+      description:
+        "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
+    },
+  ];
---
+++ src/constants/menu.js
@@ -0,0 +1,10 @@
+import { v4 } from 'uuid';
+
+
+
+export const navItems = [
+  {id:v4(), label: 'Features', href: '#' },
+  {id:v4(), label: 'Workflow', href: '#' },
+  {id:v4(), label: 'Pricing', href: '#' },
+  {id:v4(), label: 'Testimonials', href: '#' }
+];
