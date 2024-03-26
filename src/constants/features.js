f3d286961c5184e8d985459f048925f1322ac6f7
Enrique Comesaña <enriquecbar@gmail.com>
2024-03-26 17:52:36 +0100

    Features

--- src/constants/features.js
+++ src/constants/features.js
@@ -1,47 +1,39 @@
-import { BotMessageSquare } from "icons/bot-message-square.svg";
-import { BatteryCharging } from "icons/battery-charging.svg";
-import { Fingerprint } from "icons/fingerprint.svg";
-import { ShieldHalf } from "icons/shield-half.svg";
-import { PlugZap } from "icons/plug-zap.svg";
-import { GlobeLock } from "icons/globe-lock.svg";
-
-
 
 export const features = [
-    {
-      icon: <BotMessageSquare />,
-      text: "Drag-and-Drop Interface",
-      description:
-        "Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.",
-    },
-    {
-      icon: <Fingerprint />,
-      text: "Multi-Platform Compatibility",
-      description:
-        "Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.",
-    },
-    {
-      icon: <ShieldHalf />,
-      text: "Built-in Templates",
-      description:
-        "Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.",
-    },
-    {
-      icon: <BatteryCharging />,
-      text: "Real-Time Preview",
-      description:
-        "Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.",
-    },
-    {
-      icon: <PlugZap />,
-      text: "Collaboration Tools",
-      description:
-        "Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.",
-    },
-    {
-      icon: <GlobeLock />,
-      text: "Analytics Dashboard",
-      description:
-        "Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.",
-    },
-  ];
+  {
+    icon: 'icons/bot-message-square.svg',
+    text: 'Drag-and-Drop Interface',
+    description:
+      'Easily design and arrange your VR environments with a user-friendly drag-and-drop interface.'
+  },
+  {
+    icon: 'icons/battery-charging.svg',
+    text: 'Multi-Platform Compatibility',
+    description:
+      'Build VR applications that run seamlessly across multiple platforms, including mobile, desktop, and VR headsets.'
+  },
+  {
+    icon: 'icons/fingerprint.svg',
+    text: 'Built-in Templates',
+    description:
+      'Jumpstart your VR projects with a variety of built-in templates for different types of applications and environments.'
+  },
+  {
+    icon: 'icons/shield-half.svg',
+    text: 'Real-Time Preview',
+    description:
+      'Preview your VR application in real-time as you make changes, allowing for quick iterations and adjustments.'
+  },
+  {
+    icon: 'icons/plug-zap.svg',
+    text: 'Collaboration Tools',
+    description:
+      'Work together with your team in real-time on VR projects, enabling seamless collaboration and idea sharing.'
+  },
+  {
+    icon: 'icons/globe-lock.svg',
+    text: 'Analytics Dashboard',
+    description:
+      'Gain valuable insights into user interactions and behavior within your VR applications with an integrated analytics dashboard.'
+  }
+];
